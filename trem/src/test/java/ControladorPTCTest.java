3044d11ef291be564c41c1ae9d02b289755da69b
igor <asilvaigor@gmail.com>
2018-09-12 11:06:48 -0300

    Finishing test2

--- trem/src/test/java/ControladorPTCTest.java
+++ trem/src/test/java/ControladorPTCTest.java
@@ -1,10 +1,14 @@
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ControladorPTCTest {
+    private ControladorPTC controlador;
 
     @Mock
     private Sensor sensor;
@@ -15,9 +19,26 @@ public class ControladorPTCTest {
     @Mock
     private PainelCondutor painelCondutor;
 
+    @Before
+    public void setUp() {
+        controlador = new ControladorPTC(sensor, datacenter, painelCondutor);
+    }
+
     @Test
     public void testInitialization(){
-        ControladorPTC controlador = new ControladorPTC(sensor, datacenter, painelCondutor);
+
+    }
+
+    @Test
+    public void testCrossingAndSensor() {
+        when(sensor.isCruzamento()).thenReturn(true);
+        when(sensor.getVelocidade()).thenReturn(110.0);
+        when(painelCondutor.imprimirAviso("Velocidade alta", 1)).thenReturn(true);
+    }
+
+    @After
+    public void run() {
+        controlador.run();
     }
 
 }
