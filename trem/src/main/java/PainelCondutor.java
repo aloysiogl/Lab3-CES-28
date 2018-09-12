3f69d6e86c82c7369fb5740043922c569cb55fef
Unknown <aloysiogl@gmail.com>
2018-09-12 10:50:20 -0300

    add final methds in painel

--- trem/src/main/java/PainelCondutor.java
+++ trem/src/main/java/PainelCondutor.java
@@ -2,4 +2,12 @@ class PainelCondutor {
     boolean imprimirAviso(String msg, int prioridade){
         throw new java.lang.UnsupportedOperationException("Not supported yet. ");
     }
+
+    void aceleraVelocidadeTrem(double valor){
+        throw new java.lang.UnsupportedOperationException("Not supported yet. ");
+    }
+
+    void diminuiVelocidadeTrem(double valor){
+        throw new java.lang.UnsupportedOperationException("Not supported yet. ");
+    }
 }
